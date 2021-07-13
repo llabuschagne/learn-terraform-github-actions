eef2554763f2c1eb7612613ee6a2020d5a6fbed3
llabuschagne <llabuschagne@alphawave.co.za>
2021-07-13 08:51:03 +0200

    just generate an instance

--- main.tf
+++ main.tf
@@ -2,56 +2,23 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "3.26.0"
-    }
-    random = {
-      source  = "hashicorp/random"
-      version = "3.0.1"
+      version = "~> 3.27"
     }
   }
+
   required_version = "~> 1.0.2"
-
-  backend "remote" {
-    organization = "example-org-6179d9"
-
-    workspaces {
-      name = "learn-terraform-github-actions"
-    }
-  }
 }
-
 
 provider "aws" {
   profile = "awml"
   region  = "us-west-2"
 }
 
+resource "aws_instance" "app_server" {
+  ami           = "ami-270f9747"
+  instance_type = "t2.micro"
 
-
-resource "random_pet" "sg" {}
-
-resource "aws_instance" "web" {
-  ami                    = "ami-830c94e3"
-  instance_type          = "t2.micro"
-  vpc_security_group_ids = [aws_security_group.web-sg.id]
-
-  user_data = <<-EOF
-              #!/bin/bash
-              echo "Hello, World" > index.html
-              nohup busybox httpd -f -p 8080 &
-              EOF
-}
-
-resource "aws_security_group" "pypi-security-group" {
-  name = "${random_pet.sg.id}-sg"
-  ingress {
-    from_port   = 8080
-    to_port     = 8080
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+  tags = {
+    Name = "GitHub generated instance"
   }
-}
-
-output "web-address" {
-  value = "${aws_instance.web.public_dns}:8080"
 }

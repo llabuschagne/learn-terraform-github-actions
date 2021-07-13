f19d8491b787ac6203e5aa3a58292523c8d292fa
llabuschagne <llabuschagne@alphawave.co.za>
2021-07-13 08:16:22 +0200

    drop required terraform version

--- main.tf
+++ main.tf
@@ -9,7 +9,7 @@ terraform {
       version = "3.0.1"
     }
   }
-  required_version = "~> 0.14"
+  required_version = ">=0.13, <0.14" 
 
   backend "remote" {
     organization = "example-org-6179d9"
